eb98824e20e97e7cd3a7364ef5bbd51c3b33a7b9
Lars Ingebrigtsen <larsi@gnus.org>
2026-06-11 10:40:14 +0200

    Scaffolding

---
+++ fetch-dom.el
@@ -0,0 +1,25 @@
+;;; fetch-dom.el --- Fetch HTML -*- lexical-binding: t -*-
+
+;; Copyright (C) 2026 Lars Ingebrigtsen.
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+
+;; fetch-dom is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; fetch-dom is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'fetch-dom)
+
+;;; fetch-dom.el ends here
